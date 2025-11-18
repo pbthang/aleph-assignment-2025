0f455cd6b18a7f49b8c00c10b5e3662e1ceb2a5c
Ba Thang PHAM <bathang.pham@anacle.com>
2025-11-18 17:49:25 +0800

    Fix build error

--- src/components/task2/DeleteEdgeBtn.tsx
+++ src/components/task2/DeleteEdgeBtn.tsx
@@ -1,21 +1,20 @@
 import { useEdgeStore } from "@/store/edgeStore";
-import React from "react";
 import { Button } from "../ui/button";
 import { Trash2 } from "lucide-react";
 
 function DeleteEdgeBtn({ fromId, toId }: { fromId: string; toId: string }) {
-  const removeEdge = useEdgeStore((state) => state.removeEdge);
+    const removeEdge = useEdgeStore((state) => state.removeEdge);
 
-  return (
-    <Button
-      variant={"ghost"}
-      size="icon"
-      onClick={() => removeEdge(fromId, toId)}
-    >
-      <span className="sr-only">Delete node</span>
-      <Trash2 className="text-red-500" />
-    </Button>
-  );
+    return (
+        <Button
+            variant={"ghost"}
+            size="icon"
+            onClick={() => removeEdge(fromId, toId)}
+        >
+            <span className="sr-only">Delete node</span>
+            <Trash2 className="text-red-500" />
+        </Button>
+    );
 }
 
 export default DeleteEdgeBtn;
